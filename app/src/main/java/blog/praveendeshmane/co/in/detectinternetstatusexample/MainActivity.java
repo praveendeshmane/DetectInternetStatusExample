03f6d8ca018b3b5b93128288ae694ed268e9b826
praveendeshmane <deshmane.praveen@gmail.com>
2017-12-14 00:18:06 +0530

    Initial commit

---
+++ app/src/main/java/blog/praveendeshmane/co/in/detectinternetstatusexample/MainActivity.java
@@ -0,0 +1,81 @@
+package blog.praveendeshmane.co.in.detectinternetstatusexample;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity
+        implements ConnectivityReceiver.ConnectivityReceiverListener {
+
+    private Button btnCheck;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        btnCheck = findViewById(R.id.btn_check);
+
+        // Manually checking internet connection
+        checkConnection();
+
+        btnCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Manually checking internet connection
+                checkConnection();
+            }
+        });
+    }
+
+    // Method to manually check connection status
+    private void checkConnection() {
+        boolean isConnected = ConnectivityReceiver.isConnected();
+        showSnack(isConnected);
+    }
+
+    // Showing the status in Snackbar
+    private void showSnack(boolean isConnected) {
+        String message;
+        int color;
+        if (isConnected) {
+            message = "Good! Connected to Internet";
+            color = Color.WHITE;
+        } else {
+            message = "Sorry! Not connected to internet";
+            color = Color.RED;
+        }
+
+        Snackbar snackbar = Snackbar
+                .make(findViewById(R.id.fab), message, Snackbar.LENGTH_LONG);
+
+        View sbView = snackbar.getView();
+        TextView textView =  sbView.findViewById(android.support.design.R.id.snackbar_text);
+        textView.setTextColor(color);
+        snackbar.show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // register connection status listener
+        MyApplication.getInstance().setConnectivityListener(this);
+    }
+
+    /**
+     * Callback will be triggered when there is change in
+     * network connection
+     */
+    @Override
+    public void onNetworkConnectionChanged(boolean isConnected) {
+        showSnack(isConnected);
+    }
+}
---
+++ app/src/main/java/blog/praveendeshmane/co/in/detectinternetstatusexample/MyApplication.java
@@ -0,0 +1,27 @@
+package blog.praveendeshmane.co.in.detectinternetstatusexample;
+
+/**
+ * Created by Admin on 05-12-2017.
+ */
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+
+    private static MyApplication mInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mInstance = this;
+    }
+
+    public static synchronized MyApplication getInstance() {
+        return mInstance;
+    }
+
+    public void setConnectivityListener(ConnectivityReceiver.ConnectivityReceiverListener listener) {
+        ConnectivityReceiver.connectivityReceiverListener = listener;
+    }
+}
